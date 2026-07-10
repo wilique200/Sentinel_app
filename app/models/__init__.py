f4b7bba69e4f2507965e3ca68744b3ead5837a0c
wilique200 <wilique200@gmail.com>
2026-07-10 19:43:42 +0100

    Delete app/models/__init__.py

--- app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.models.user import User
-from app.models.location import SavedLocation
-from app.models.prediction import PredictionSnapshot
-from app.models.chat import ChatMessage
-
-__all__ = ["User", "SavedLocation", "PredictionSnapshot", "ChatMessage"]
