705cc0da81c4bb3b3603cb6590b111b107efec3b
wilique200 <wilique200@gmail.com>
2026-07-12 16:13:55 +0100

    Delete app/schemas/__init__.py

--- app/schemas/__init__.py
+++
@@ -1,7 +0,0 @@
-from app.schemas.auth import UserSignup, UserLogin, UserOut, Token
-from app.schemas.prediction import LocationQuery, GeocodeCandidate, PredictRequest, HazardWarning, PredictResponse
-
-__all__ = [
-    "UserSignup", "UserLogin", "UserOut", "Token",
-    "LocationQuery", "GeocodeCandidate", "PredictRequest", "HazardWarning", "PredictResponse",
-]
